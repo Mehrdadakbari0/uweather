a18eb22ba1b279cbef03ca263ddf4661225b1441
mehrdadakbari0 <$mehrdadakbaribahra@gamil.com>
2024-09-17 22:53:08 +0330

    Update

---
+++ components/Pages/index.tsx
@@ -0,0 +1,193 @@
+import Component, { PageEl } from '@/components/Libs/Component';
+import Copy from '@/components/Libs/Copy';
+import Router from 'next/router'
+import Window from '@/components/Libs/Window';
+import TextBox from '@/components/Libs/TextBox';
+import Icon2Titles from '@/components/Libs/Icon2Titles';
+import Icon3Titles from '@/components/Libs/Icon3Titles';
+import { time } from 'console';
+
+
+export default p => Component(p, Page);
+const Page: PageEl = (props, state, refresh, getProps) => {
+
+  let styles = global.styles
+  let name = ""
+
+
+
+
+
+  return (
+    <div style={{ direction: "ltr", minHeight: "11vh", }}>
+      <br-x />
+      <Window title={name} style={{
+        minHeight: 200, margin: 10, width: "calc(100% - 20px)",
+        backgroundImage: 'url("https://cdn.turing.team/research/0/bg.webp")',
+
+      }}>
+        {/* <pre style={{ direction: "rtl" }}>{JSON.stringify(props, null, 2)}</pre> */}
+
+
+        <c-x >
+          <br-x />
+          <br-x />
+          <br-x />
+          <f-csb style={{ padding: "0 20px", color: "lightsteelblue", fontSize: 30 }}>   weather:  </f-csb>
+          <br-x />
+
+          <f-cc style={{ padding: "0 50px", color: "white", fontSize: 15 }}> {props.date} </f-cc>
+          <f-cc style={{ padding: "0 50px", color: "white", fontSize: 15 }}>{props.time}</f-cc>
+
+
+
+          <f-cse>
+
+
+            <f-cc style={{ height: 80, width: 300, backgroundColor: "lightgray", borderRadius: 15 }}>
+              <img src="https://cdn.turing.team/research/56/temp.webp"
+                style={{ height: 30, objectFit: "contain" }} />
+              <sp-5 />
+
+              <f-cc>
+                <f-14>Feels Like:</f-14>
+                <sp-1 />
+                <f-14> {props.feelslikec}</f-14>
+                <f-14>  °     </f-14>
+                <f-14> C </f-14>
+              </f-cc>
+
+
+            </f-cc>
+
+            <f-cc style={{ height: 80, width: 300, backgroundColor: "lightgray", borderRadius: 15 }}>
+              <img src="https://cdn.turing.team/research/56/temp.webp"
+                style={{ height: 30, objectFit: "contain" }} />
+              <sp-2 />
+              <f-cc>
+                <f-14>درجه هوا:</f-14>
+                <sp-1 />
+                <f-14>{props.feelslikec}</f-14>
+                <f-14>  °     </f-14>
+                <f-14> C </f-14>
+              </f-cc>
+            </f-cc>
+
+          </f-cse>
+          <br-x />
+          <f-cse>
+            <f-cc style={{ height: 80, width: 300, backgroundColor: "lightgray", borderRadius: 15 }}>
+              <img src="https://cdn.turing.team/research/56/i.png"
+                style={{ height: 30, objectFit: "contain" }} />
+              <sp-2 />
+              <f-cc>
+                <f-14>pressure:</f-14>
+                <sp-1 />
+                <f-14>{props.pressure}</f-14>
+              </f-cc>
+
+            </f-cc>
+            <f-cc style={{ height: 80, width: 300, backgroundColor: "lightgray", borderRadius: 15 }}>
+              <img src="https://cdn.turing.team/research/56/i.png"
+                style={{ height: 30, objectFit: "contain" }} />
+              <sp-2 />
+              <f-cc>
+                <f-14>فشار:</f-14>
+                <sp-1 />
+                <f-14>{props.pressure}</f-14>
+              </f-cc>
+            </f-cc>
+
+          </f-cse>
+          <br-x />
+          <f-cse>
+
+            <f-cc style={{ height: 80, width: 300, backgroundColor: "lightgray", borderRadius: 15 }}>
+              <img src="https://cdn.turing.team/research/56/Hu.webp"
+                style={{ height: 30, objectFit: "contain" }} />
+              <sp-2 />
+              <f-cc>
+                <f-14>Humidity:</f-14>
+                <sp-1 />
+                <f-14>{props.humidity}</f-14>
+                <f-14>  %  </f-14>
+                <f-14> RH </f-14>
+              </f-cc>
+            </f-cc>
+
+            <f-cc style={{ height: 80, width: 300, backgroundColor: "lightgray", borderRadius: 15 }}>
+              <img src="https://cdn.turing.team/research/56/Hu.webp"
+                style={{ height: 30, objectFit: "contain" }} />
+              <sp-2 />
+              <f-cc>
+                <f-14>رطوبت هوا:</f-14>
+                <sp-1 />
+                <f-14>{props.humidity}</f-14>
+                <f-14>  %  </f-14>
+                <f-14> RH </f-14>
+              </f-cc>
+            </f-cc>
+          </f-cse>
+
+
+
+          <br-x />
+          <f-cc style={{ width: "100%", color: "white" }}>
+            <f-13>تهیه شده توسط  گروه پژوهشی تورینگ </f-13>
+          </f-cc>
+
+        </c-x>
+
+
+
+        {/*  <div style={{ direction: "ltr", fontSize: 15 }}>
+          <span>Feels like:  {props.feelslikec} °C</span>
+          <br />
+          <span>Humidity:  {props.humidity} % RH </span>
+          <br />
+          <span>Pressure:  {props.pressure} atm</span>
+          <br />
+          <span> server date:  {props.date}</span>
+          </div> */}
+
+
+
+      </Window>
+    </div >
+  )
+}
+
+
+export async function getServerSideProps(context) {
+
+
+  var session = await global.SSRVerify(context)
+  var { uid, name, image, imageprop, lang, cchar,
+    unit, workspace, servid, servsecret,
+    usedquota, quota, quotaunit, status, regdate, expid,
+    role, path, devmod, userip, } = session;
+
+
+  let data = await (await fetch("https://cdn.turing.team/research/api/weather")).json()
+
+  let feelslikec = data.current_condition[0].FeelsLikeC
+  let humidity = data.current_condition[0].humidity
+  let pressure = data.current_condition[0].pressure
+
+  let date = new Date().toLocaleDateString()
+  let time = new Date().toLocaleTimeString()
+
+  return {
+    props: {
+      data: global.QSON.stringify({
+        session,
+        feelslikec,
+        humidity,
+        pressure,
+        date,
+        time,
+        // nlangs,
+      })
+    },
+  }
+}
